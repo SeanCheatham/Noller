6e293b3a0ce665bbdd5fa7a11ac1c83bd65dc03d
SeanCheatham <smcheath@syr.edu>
2014-12-18 18:48:22 -0500

    New Algorithm

--- cruncher2.py
+++ cruncher2.py
@@ -18,96 +18,90 @@ CYCLE = 1
 
 # Helper Functions
 def populateData():
-    global things, cur
+    global things, cur, world
+    world = []
+    for i in range (0,1000):
+        inner = []
+        for j in range (0,1000):
+            inner.append(None)
+        world.append(inner)
 
     cur.execute("SELECT * FROM things")
     dbThings = cur.fetchall()
     for dbThing in dbThings:
-        things[dbThing['id']] = {'pos': dbThing['pos'], 'force': dbThing['force'], 'mass': dbThing['mass']}
-
+        if(dbThing['posX'] is None or dbThing['posY'] is None):
+            coords = findRandomEmptyLocation()
+            dbThing['posX'] = coords[0]
+            dbThing['posY'] = coords[1]
+        thing = {'posX': dbThing['posX'], 'posY': dbThing['posY'], 'forceX': 0.0, 'forceY': 0.0}
+        things[dbThing['id']] = thing
+        world[thing['posX']][thing['posY']] = thing
     cur.execute("SELECT * FROM relations")
     dbRelations = cur.fetchall()
     for dbRelation in dbRelations:
         relations.append({'parent': dbRelation['parent'], 'child': dbRelation['child'], 'value': dbThing['value']})
 
+def findRandomEmptyLocation():
+    random.seed()
+    randX = math.floor(random.random() * len(world))
+    randY = math.floor(random.random() * len(world[randX]))
+    if world[randX][randY] is None:
+        return (randX,randY)
+    else:
+        return findRandomEmptyLocation()
+
+def findEmptyLocationAroundPoint(x, y, radius):
+    if(world[x][y] is None):
+        return (x,y)
+    for i in random.shuffle(range(x - radius, x + radius)):
+        if i < 0 or i >= len(world):
+            continue
+        for j in random.shuffle(range(y - radius, y + radius)):
+            if j < 0 or j > len(world[i])-1:
+                continue
+            if(world[i][j] is None):
+                return (i,j)
+    return findEmptyLocationAroundPoint(x, y, radius + 1)
 
 #Calculation Function
-def calculate(indices):
+def calculateAttractions(indices):
     global things, relations
-    # Calculate force, acceleration, velocity, and position for each thing
-    for idx in indices:
-        direction = 1 if things[idx] > 0 else -1
-        scalar = direction * CENTER_REPULSION *
+    for r in indices:
+        deltaX = things[relations[r]['parent']]['posX'] - things[relations[r]['child']]['posX']
+        deltaY = things[relations[r]['parent']]['posY'] - things[relations[r]['child']]['posY']
+        direction = math.atan2(deltaY, deltaX)
+        distance = math.pow(deltaX,2) + math.pow(deltaY,2)
+        afScalar = GRAVITY * (1/r.value) * distance
+        things[[r]['parent']]['forceX'] -= afScalar * math.cos(direction)
+        things[[r]['parent']]['forceY'] -= afScalar * math.sin(direction)
 
-        # Force
-        #Repulsive Force from center
-        # f = ma, but consider "CENTER_REPULSION" to be "mass" since this is a fake force. a = 1/r^2 * direction
-
-        direction = math.atan2(thing.posY, thing.posX)
-        #distance = math.pow(thing.posX,2) + math.pow(thing.posY,2)
-        #distance = distance if distance > 0 else 0.00001
-        #fScalar = CENTER_REPULSION * (1/math.pow(distance,2))
-        cfScalar = CENTER_REPULSION
-        thing.forceX += cfScalar * math.cos(direction)
-        thing.forceY += cfScalar * math.sin(direction)
-
-        for thing2 in things:
-            if thing2 == thing or thing2 in thing.relations: continue
-            deltaX = thing.posX - thing2.posX
-            deltaY = thing.posY - thing2.posY
-            direction = math.atan2(deltaY, deltaX)
-            distance = math.pow(deltaX,2) + math.pow(deltaY,2)
-            distance = max(distance, 0.001)
-            rfScalar = PARTICLE_REPULSION * (1/math.pow(distance,2))
-            thing.forceX += rfScalar * math.cos(direction)
-            thing.forceY += rfScalar * math.sin(direction)
-
-
-        # Attractive force for each related thing
-        #print len(thing.relations)
-        for relatedThing in thing.relations:
-            deltaX = thing.posX - relatedThing.child.posX
-            deltaY = thing.posY - relatedThing.child.posY
-            direction = math.atan2(deltaY, deltaX)
-            distance = math.pow(deltaX,2) + math.pow(deltaY,2)
-            #fScalar = GRAVITY * (thing.m if thing.m > 0 else 0.1) * relatedThing.child.m * math.pow(distance,2)
-            afScalar = GRAVITY * (1/relatedThing.value) * math.pow(distance,2)
-            #print thing.id,relatedThing.child.id,fScalar
-            # f = ma = G(m1)(m2)(radius)^2 * direction
-            thing.forceX -= afScalar * math.cos(direction)
-            thing.forceY -= afScalar * math.sin(direction)
-
-        # Acceleration
-        thing.accX = thing.forceX #/ (thing.m if thing.m > 0 else 0.1)
-        thing.accY = thing.forceY #/ (thing.m if thing.m > 0 else 0.1)
-
-        # Velocity
-        thing.velX = thing.accX * dt
-        thing.velY = thing.accY * dt
-
-        # Position
-        thing.posX += thing.velX
-        thing.posY += thing.velY
-
-        #print thing.forceX,thing.forceY,thing.accX,thing.accY,thing.velX,thing.velY,thing.posX,thing.posY
-
+def moveParticles():
+    global things, world
+    for t in things:
+        targetX = math.floor(things[t]['posX'] + things[t]['forceX'])
+        targetY = math.floor(things[t]['posY'] + things[t]['forceY'])
+        coordinates = findEmptyLocationAroundPoint(targetX, targetY, 1)
+        world[things[t]['posX']][things[t]['posY']] = None
+        world[coordinates[0]][coordinates[1]] = things[t]
+        things[t]['posX'] = coordinates[0]
+        things[t]['posY'] = coordinates[1]
 
 def threadedCalculator(dt):
-    random.shuffle(things)
+    random.shuffle(relations)
     threads = []
-    t1 = threading.Thread(name='t1', target=calculate, args=(dt,0,len(things)/4-1))
+    t1 = threading.Thread(name='t1', target=calculateAttractions, args=(range(0,len(relations)/4-1)))
     threads.append(t1)
     t1.start()
 
-    t2 = threading.Thread(name='t2', target=calculate, args=(dt,len(things)/4,len(things)/2-1))
+    t2 = threading.Thread(name='t2', target=calculateAttractions, args=(range(len(relations)/4,len(relations)/2-1)))
     threads.append(t2)
     t2.start()
 
-    t3 = threading.Thread(name='t3', target=calculate, args=(dt,len(things)/2,len(things)/4*3-1))
+    t3 = threading.Thread(name='t3', target=calculateAttractions, args=(range(len(relations)/2,len(relations)/4*3-1)))
     threads.append(t3)
     t3.start()
 
-    t4 = threading.Thread(name='t4', target=calculate, args=(dt,len(things)/4*3,len(things)-1))
+    t4 = threading.Thread(name='t4', target=calculateAttractions, args=(range(len(relations)/4*3,len(relations)-1)))
     threads.append(t4)
     t4.start()
 
@@ -116,27 +110,31 @@ def threadedCalculator(dt):
     t3.join()
     t4.join()
 
+    moveParticles()
+
 
 #Update Database
 def updateDB():
     global cur, things
     print "Updating DB at ",datetime.now()
     i = 0
-    for thing in things:
+    for t in things:
         cur.execute("""
-            UPDATE things2
-            SET posX = %s, posY = %s, mass = %s
+            UPDATE things
+            SET posX = %s, posY = %s
             WHERE id = %s
-        """,(str(thing.posX), str(thing.posY), str(thing.m) if thing.m > 0.1 else None, str(thing.id)))
+        """,(str(things[t]['posX']), str(things[t]['posY']), str(t)))
         if(i%10 == 0): con.commit()
         i += 1
     con.commit()
     print "Finished Updating DB"
 
 def refreshData():
-    global things
+    global things, relations, world
     print "Refreshing data at ",datetime.now()
     things = {}
+    relations = []
+    world = None
     populateData()
 
 def exitCruncher():
@@ -160,12 +158,12 @@ con = None
 cur = None
 things = {}
 relations = []
+world = None
 dbConnect()
 refreshData()
 
 print "Starting calculator at ",datetime.now()
 while ( True ):
-    zeroVals()
     threadedCalculator(1)
     if(CYCLE%800 == 0):
         print "Halting calculator at ",datetime.now()
